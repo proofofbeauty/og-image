3a0bbddf974158e5e9ce4533c6786ecab827ea7b
David Sun <me@bydavidsun.com>
2021-03-01 10:00:57 -0600

    ADD cors

--- api/index.ts
+++ api/index.ts
@@ -6,7 +6,25 @@ import { ArtParsedRequest, DefaultParsedRequest, PrintArtParsedRequest, PaletteP
 const isDev = !process.env.AWS_REGION;
 // const isHtmlDebug = process.env.OG_HTML_DEBUG === '1';
 
-export default async function handler(req: IncomingMessage, res: ServerResponse) {
+const allowCors = (fn: any) => async (req: IncomingMessage, res: ServerResponse) => {
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+        res.statusCode = 200;
+      res.end()
+      return
+    }
+    return await fn(req, res)
+  }
+
+export default allowCors(async function handler(req: IncomingMessage, res: ServerResponse) {
     try {
         const parsedRequest = parseRequest(req);
 
@@ -37,4 +55,4 @@ export default async function handler(req: IncomingMessage, res: ServerResponse)
         res.setHeader('Content-Type', 'text/html');
         res.end('<h1>Internal Error</h1><p>Sorry, there was a problem</p>');
     }
-}
+})
